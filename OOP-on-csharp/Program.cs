bc2b238a03c7d46bef769595cd958bfe8dc121ab
themainikaru <themainikaru@gmail.com>
2025-10-02 17:01:54 +1000

    KT7 Struct // 3.7

--- OOP-on-csharp/Program.cs
+++ OOP-on-csharp/Program.cs
@@ -6,34 +6,42 @@ namespace OOP_on_csharp
     {
         static void Main(string[] args)
         {
-            Date birthday = new Date(8, 1, 1984);
+            PositionVector vector0 = new PositionVector(10, 20);
+            PositionVector vector1 = new PositionVector(30, 40);
 
-            Console.WriteLine(birthday.IsLeapYear());
+
+            Console.WriteLine($"vector 1 length is {vector0.GetLength()}");
+            Console.WriteLine($"vector 2 length is {vector1.GetLength()}");
+
+            Console.WriteLine($"\nvector 1:\n{vector0.Normalize()}");
+            Console.WriteLine($"\nvector 2:\n{vector1.Normalize()}");
+
+            Console.WriteLine($"\nvector 1 and vector 2 dot product is {PositionVector.GetDotProduct(vector0, vector1)}");
         }
     }
 
-    public struct Date
+    public struct PositionVector
     {
-        private byte _day;
-        private byte _month;
-        private short _year;
-
-        public Date(byte day, byte month, short year)
+        public double X, Y;
+        public PositionVector(double x, double y)
         {
-            _day = day;
-            _month = month;
-            _year = year;
+            X = x;
+            Y = y;
         }
 
-        public bool IsLeapYear()
+
+        public double GetLength()
         {
-            switch (_year % 4)
-            {
-                case 0:
-                    return true;
-                default:
-                    return false;
-            }
+            return Math.Sqrt((X * X) + (Y * Y));
+        }
+        public static double GetDotProduct(PositionVector v1, PositionVector v2)
+        {
+            return v1.X * v2.X + v1.Y * v2.Y;
+        }
+
+        public string Normalize()
+        {
+            return $"normalized X is {X / GetLength()}\nnormalized Y is {Y / GetLength()}";
         }
     }
 }
