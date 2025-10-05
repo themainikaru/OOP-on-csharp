7500d5ff9cce31d303de971901369792ea7f6232
themainikaru <themainikaru@gmail.com>
2025-10-05 17:34:02 +1000

    KT7 Struct // 3.9

--- OOP-on-csharp/Program.cs
+++ OOP-on-csharp/Program.cs
@@ -6,35 +6,12 @@ namespace OOP_on_csharp
     {
         static void Main(string[] args)
         {
-            Money amount0 = new Money(111, 222);
-            Money amount1 = new Money(333, 444);
+            Triangle.Point pointA = new Triangle.Point(0, 0);
+            Triangle.Point pointB = new Triangle.Point(3, 0);
+            Triangle.Point pointC = new Triangle.Point(0, 4);
 
-            Console.WriteLine(Money.Sum(amount0, amount1));
-        }
-    }
-
-    public struct Money
-    {
-        public int Roubles;
-        public int Kopecks;
-        public Money(int roubles, int kopecks)
-        {
-            Roubles = roubles;
-            Kopecks = kopecks;
-        }
-
-        public static string Sum(Money amount1, Money amount2)
-        {
-            long roubles = amount1.Roubles + amount2.Roubles;
-            long kopecks = amount1.Kopecks + amount2.Kopecks;
-
-            if (kopecks >= 100)
-            {
-                roubles += kopecks / 100;
-                kopecks = kopecks % 100;
-            }
-
-            return $"{roubles} рублей {kopecks} копеек";
+            Triangle triangle = new Triangle(pointA, pointB, pointC);
+            triangle.PrintInfo();
         }
     }
 }
