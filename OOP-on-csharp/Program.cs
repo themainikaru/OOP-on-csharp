5bae2e2cf78e629ba65abe433c061c8148311c67
themainikaru <themainikaru@gmail.com>
2025-11-16 17:35:02 +1000

    KT3 access modifiers

--- OOP-on-csharp/Program.cs
+++ OOP-on-csharp/Program.cs
@@ -2,15 +2,55 @@
 
 namespace OOP_on_csharp
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Box<int> box1 = new Box<int>(888);
-            Box<string> box2 = new Box<string>("string");
+            Student student = new Student();
 
-            Console.WriteLine($"число в коробке: {box1.Get()}");
-            Console.WriteLine($"строка в коробке: {box2.Get()}");
+            student.SetFullName("Иванов Иван Иванович");
+            student.SetGroupName("СОИП24-ПР1");
+            student.SetAge(17);
+            student.SetParentsFullName("*ФИО родителя*");
+
+            student.PrintInfo();
+        }
+    }
+
+
+    public class Group
+    {
+        protected internal string _groupName;
+    }
+
+
+    public class Student : Group
+    {
+        private string FullName;
+        private byte _age;
+        private string _parentsFullName;
+        internal byte inClassСorrectAnswers;
+
+        public void SetFullName(string fullName)
+        {
+            FullName = fullName;
+        }
+        public void SetGroupName(string groupName)
+        {
+            _groupName = groupName;
+        }
+        public void SetAge(byte age)
+        {
+            _age = age;
+        }
+        public void SetParentsFullName(string parentsFullName)
+        {
+            _parentsFullName = parentsFullName;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{FullName}\t{_groupName}\t{_age}\t{_parentsFullName}");
         }
     }
 }
